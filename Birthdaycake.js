024b1be50b56bd8a94bf7adf225cab1799077006
hafidhsajid <hafidhsajid@gmail.com>
2023-02-10 16:25:05 +0700

    Add birthday cake case

---
+++ Birthdaycake.js
@@ -0,0 +1,25 @@
+var candles = [3,2,1,3,9,3,4,4,4,4,4,4,4]
+var most=[]
+candles.forEach((e)=>{
+    count = 0
+    candles.forEach((e1)=>{
+        if (e==e1) {
+            count++
+        }
+    })
+    most.push({count:count, arr:e})
+})
+var res
+var rescount
+most.forEach((e)=>{
+    if (res==undefined) {
+        res = e.arr
+        rescount = e.count
+    }
+    if (rescount<e.count) {
+        res = e.arr
+        rescount = e.count
+        
+    }
+})
+console.log(`${res},${rescount}`);
